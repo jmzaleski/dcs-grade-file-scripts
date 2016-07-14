8f805bee297283856cb770f59260c075a3ab1914
Mathew Zaleski <matz@zaleski.ca>
2016-07-14 17:21:30 -0400

    make blanks go away in mailto: URL

--- student.py
+++ student.py
@@ -22,13 +22,12 @@ else:
     msg.warning( "usage: ", sys.argv[0], " class-list-file-from-CDF grades-empty query string")
     exit(2)
 
-empty_reader = grade_file_reader.GradeFileReader(EMPTY_GRADES)    
-empty_reader.skipHeader()
-
 #build maps from cdfid (which we can always parse out of these files)
 cdfid_to_tut = {}
 cdfid_to_empty_grades_file_line = {}
 
+empty_reader = grade_file_reader.GradeFileReader(EMPTY_GRADES)    
+empty_reader.skipHeader()
 for line in empty_reader.readLines():
     (dropped, flag_char, cdfid, sec, ta) =  empty_reader.parseEmptyGradeFileLine(line)
     cdfid_to_tut[cdfid] = ta
@@ -37,11 +36,9 @@ for line in empty_reader.readLines():
 msg.debug(cdfid_to_tut)
 
 #read the CDF class list looking for lines that match the query
-class_list_reader = cdf_class_list_reader.CdfClassListFileReader(CLASS_LIST_FILE_NAME)
-
-lines = class_list_reader.readLines()    
 matched_lines = []
-for line in lines:
+class_list_reader = cdf_class_list_reader.CdfClassListFileReader(CLASS_LIST_FILE_NAME)
+for line in class_list_reader.readLines() :
     line = line.rstrip()
     m = re.search(query_string, line)
     if m:
@@ -51,15 +48,12 @@ if len(matched_lines) == 0 :
     msg.error("failed to find any lines in", CLASS_LIST_FILE_NAME,"MATCHING", query_string)
 
 menu = MatzMenu(matched_lines,"select a match")
-#line = matched_lines[menu(matched_lines, "select a match: ")].rstrip()
 line = matched_lines[menu.menu()].rstrip()
-
-msg.debug("hit selected=", line)
 
 student = class_list_reader.parseClassListLine(line)
 cdfid = student.cdfid
 name = student.name
-email= student.email
+email= student.email.strip()
 
 if cdfid in cdfid_to_tut:
     ta = cdfid_to_tut[cdfid]
@@ -68,25 +62,30 @@ else:
         ta = cdfid_to_tut[cdfid+"__"]
     else:
         msg.error("cdf key", cdfid, "in grades file but not in  class list")
-msg.debug(cdfid, name, email,ta)
 
-mail_to = "mailto:"+ email
-strs_to_copy = [line, cdfid, name, email, mail_to, ta]
+eline = cdfid_to_empty_grades_file_line[cdfid]
+
+#array in same order as menu
+strs_to_copy = [line, eline, cdfid, name, email, ta, "MAILTO:", "IMAGE:"]
 
 menu_lines = ["CLINE " + line,
-              "ELINE " + cdfid_to_empty_grades_file_line[cdfid], 
-              "CDFID " + cdfid, 
+              "ELINE " + eline,
+              "CDFID " + cdfid,
               "NAME  " + name, 
               "EMAIL"  + email,
-              mail_to, 
-              "TA    " + ta]
-m2 = MatzMenu(menu_lines, "option: ")
-#str_to_clipboard = strs_to_copy[menu(menu_lines, "select what to copy to clipboard: ")]
+              "TA    " + ta,
+              "MAILTO:",
+              "IMAGE:"]
+m2 = MatzMenu(menu_lines, "option: ") #number of selected menu item
+
 str_to_clipboard = strs_to_copy[m2.menu()]
 
 msg.debug("string to be copied to clipboard=", str_to_clipboard)
 
-if str_to_clipboard == mail_to:
-    os.system('open "%s"' % str_to_clipboard)
+if str_to_clipboard == "MAILTO:":
+    print('open mailto:%s' % email)
+    os.system('open mailto:%s' % email)
+elif str_to_clipboard == "IMAGE:":
+    os.system('open pics/%s.jpg' % cdfid)
 else:
     os.system("/bin/echo -n '%s' | pbcopy" % str_to_clipboard)
