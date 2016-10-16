d71b03f594837a30ab59dc9da54ab5c243304016
Mathew Zaleski <matz@zaleski.ca>
2016-10-16 15:37:04 -0400

    change to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,32 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(name='grade-file-tools',
       version='1.0',
       py_modules=['record-mark-section'],
-      )
+classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    ],
+      install_requires=['readline','csv','argparse'],
+
+    scripts=['record-mark-section.py'],
+)
