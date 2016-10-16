2d2d4683f96b14290a6decd1f032c223d80aca9e
Mathew Zaleski <matz@zaleski.ca>
2016-10-16 16:24:32 -0400

    added the imported stuff to py_modules thingy

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
-#from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(name='grade-file-tools',
       version='1.0',
-      py_modules=['record-mark-section'],
-classifiers=[
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
-    'Development Status :: 3 - Alpha',
+      py_modules=['record-mark-section','menu','set_up_readline_for_completion','complete','grade_file_reader_writer'],
+
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
 
     # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
 
     # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: MIT License',
 
     # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    ],
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
+      
       install_requires=['argparse'],
 
-    scripts=['record-mark-section.py'],
+      scripts=['record-mark-section.py'],
 )
