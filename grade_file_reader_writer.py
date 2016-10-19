57d25d955ea45e5f96f59200d2fdfdd9e06c3ce6
Mathew Zaleski <matz@zaleski.ca>
2016-10-19 11:49:23 -0400

    bogusly hacky. need to refactor into subclass pronto.

--- grade_file_reader_writer.py
+++ grade_file_reader_writer.py
@@ -1,4 +1,5 @@
 import re
+from Namespace import Namespace
 
 class GradeFileReaderWriter(object):
     """read a Jim Clarke style grades file and squirrel away the data for later.
@@ -10,37 +11,83 @@ class GradeFileReaderWriter(object):
         self.grade_file_name = fn
         self.line_array = []
         self.line_value_index = {}
+        #list of marks in order they were found in the grade file
+        self.mark_names = ["student_no"]
+        self.mark_definitions = {"student_no":None}
+        self.field_number_of_mark_definition = {}
+        self.separator = None #maybe should say tab or whatever default is in grade file
+        self.students = []
         self.read_file()
         return
 
     def read_file(self):
         """
          reads the lines out of the grades file, including the header.
+         See http://www.cdf.toronto.edu/~clarke/grade/fileformat.shtml
         :return:
         """
         try:
+            #open read binary so as to not mess up the line endings and whatnot
             with open(self.grade_file_name, 'rb') as grade_file:
                 grade_file = open(self.grade_file_name, 'rb')
-                ix = 0
-                hdr = True
-                for bline in grade_file:
+                #first three chars of file may set separator character for marks
+                first_line = grade_file.readline().decode('UTF-8')
+                self.line_array.append(first_line)
+
+                if first_line.startswith("*/"):
+                    assert len(first_line)>3
+                    self.separator = first_line[2]
+                    print("found separator", self.separator)
+
+                # scan lines in file header looking for mark defintions
+                ix_mark_defn = 0
+                ix_line_number = 1
+                for bline in grade_file: # examine header of file
                     line = bline.decode('UTF-8').rstrip('\n')
                     if len(line) == 0:
-                        hdr = False
+                        break #empty line ends header
+                        self.line_array.append(line)
+                        #self.line_value_index[line] = ix  # remember the spot in line_array..
+                    ix_line_number += 1
+                    vals = line.split()
+                    if vals[0].startswith("*"):
+                        continue # comments not interesting
+                    #eg: a1 / 10
+                    mark_defn_name = vals[0]
+                    self.mark_names.append(mark_defn_name)
+                    mark_decl = vals[1:]
+                    self.mark_definitions[mark_defn_name] = mark_decl
+                    self.field_number_of_mark_definition[mark_defn_name] = ix_mark_defn
+                    ix_mark_defn += 1
+
+                print(self.mark_names)
+                print(self.mark_definitions)
+                print(self.field_number_of_mark_definition)
+
+                #squirrel away rest of file
+                for bline in grade_file:
+                    line = bline.decode('UTF-8').rstrip('\n')
+                    assert len(line) != 0
                     self.line_array.append(line)
-                    self.line_value_index[line] = ix  # remember the spot in line_array..
-                    ix += 1
-                    if hdr:
-                        vals = line.split()
-                        print("line:", line)
-                        print("vals", vals)
-                        if vals[0].startswith("*"):
-                            print("comment", line)
-                        else:
-                            print("var", vals[0])
+                    self.line_value_index[line] = ix_line_number  # remember the spot in line_array..
+                    vals = line.split(self.separator)
+                    ns = Namespace().init_names(self.mark_names)
+                    ix = 0
+                    #TODO: how would a cool kid do this?
+                    for name in self.mark_names:
+                        if ix < len(vals):
+                            ns.set(name,vals[ix])
+                        ix +=1
+                    self.students.append(ns)
+                    ix_line_number += 1
+
                 grade_file.close()
+                return self
         except:
             raise Exception("GradeFileReader fails to read " + self.grade_file_name)
+
+        return self
+
 
     def matching_lines(self, query):
         "return lines that match the query"
@@ -73,3 +120,12 @@ class GradeFileReaderWriter(object):
         with open(fn,'w') as new_file:
             for l in self.line_array:
                 print(l, file=new_file)
+
+if __name__ == '__main__':
+    gfr = GradeFileReaderWriter("/tmp/CSC300H1F-empty").read_file()
+    #list comprehensions considered powerful!
+    for ta in set([ns.ta for ns in gfr.students]):
+        print(ta,[ns.utorid for ns in gfr.students if ns.ta == ta])
+
+
+
