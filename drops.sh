3e76410559a5e1140bf1089ab36e9e49d13933c0
Mathew Zaleski <matz@zaleski.ca>
2016-11-07 11:01:40 -0500

    drops works with new classlists now

--- drops.sh
+++ drops.sh
@@ -6,7 +6,7 @@ die(){ echo $*;	exit; }
 GBIN=../../grades-bin
 test -d $GBIN || die no grades bin in $GBIN
 
-test ! -z "$1" || die usage: $0 marks file
+test ! -z "$1" || die usage: $0 marks file '#assumes classlist updated already, perhaps using new-classlist.sh'
 test -f $1 || die $0: $1 not a marks file
 
 mf=$1
@@ -21,34 +21,36 @@ test -f $CLASSLIST || die $0: cannot find class list $CLASSLIST
 
 #### assumes that the classlist has been updated already
 
+#this is where we we'll mangle marks file
 TMP_MF=/tmp/$mf$$
-TMP_CL=/tmp/cl$$
 
-echo have to test rest of this this for new CDF class list format.
-exit 2
-#something like 
+TMP_CL=/tmp/cl$$
+#will cut student numbers out of class list and put them in file of their own. hence in column 0
 cut -d, -f2 $CLASSLIST > $TMP_CL
 
 cp $mf $TMP_MF
 
-
-# -c 9 something about 9 vs 10 digit student numbers
-# since our students are currently a mixture,
-# using -c 10 will ignore the leading 1 of the 10 digit numbers..
-# which is okay, I guess.
-# more annoying is that the gupdatedrops rewrites the grades file with 10 digit numbers.
-#
-#CMD="$GBIN/gupdatedrops -c 9 -L $CLASSLIST"
-CMD="$GBIN/gupdatedrops -c 9 -L $TMP_CL"
+# -c 0 tells it to look for student numbers at left margin.. which makes sense given cut command above
+# more problematic is -9 vs -10
+# http://www.cdf.toronto.edu/~clarke/grade/lineapps/gupdatedrops.shtml suggests we use -9
+CMD="$GBIN/gupdatedrops -c 0 -L $TMP_CL"
 
 #this sets the drop column for gonzo students
-$CMD $TMP_MF || die "failed to run $CMD $TMP_MF"
 
 set -x
-diff -w $TMP_MF $mf
+$CMD $TMP_MF || die "failed to run $CMD $TMP_MF"
+
+diff $mf $TMP_MF
 set -
 
+echo
+echo NOTE gupdate drops rewrites 9 digit student numbers as 10, hence diff hard useless
+echo reasonable hack is to s/^9/09/ in grades file beforehand
+echo if too complicated try:
+echo xdiff $mf $TMP_MF
 read -p "look right? interrupt out now. enter to apply same drops to $mf  >" junk
+
+
 bf=$(backfilename.sh $mf)
 cp $mf $bf
 ls -l $bf
