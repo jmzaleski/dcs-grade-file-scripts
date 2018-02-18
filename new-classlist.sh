42354675edcb26608fa0f03a23ecb4789935e472
Mathew Zaleski <matz@zaleski.ca>
2018-02-18 12:29:08 -0500

    new and improved after csc300 last year

--- new-classlist.sh
+++ new-classlist.sh
@@ -1,46 +1,80 @@
 #!/bin/bash
 
-# print out the marks in a marks file that are "missing".. zero or blank.
-# challenge is that gpr has no way of selecting blank marks
+# fetch a new classlist from CDF
 
 die(){ echo $*;	exit; }
-
-GBIN=../../grades-bin
-
-test -d $GBIN || die no grades bin in $GBIN
 
 test -f ./dot.sh || die cannot find ./dot.sh
 source ./dot.sh
 
 test ! -z "$COURSE" || die COURSE env not found.. should be defined in ./dot.sh
 test ! -z "$SESSION" || die SESSION env not found.. should be defined in ./dot.sh
+test -d $GBIN || die no grades bin in $GBIN
 
-CLASSLIST="CSC"$COURSE$SESSION
+CDF="CSC"$COURSE$SESSION
+CLASSLIST=$CDF-classlist
+EMPTY=$CDF-empty
+
 test -f $CLASSLIST || die $0: cannot find class list $CLASSLIST
 
 TMP=/tmp/CL$$
 TMP_SORT=/tmp/CL-sorted$$
 
-# scp matz@cdf.utoronto.ca:/u/csc/instructors/classlists/$CDF $TMP_CLASSLIST
-# ugh. what i call a classlist they call an emaillist
-#
 set -x
-scp matz@cdf.utoronto.ca:/u/csc/instructors/emaillists/$CLASSLIST $TMP || die scp failed
+scp matz@cdf.utoronto.ca:/u/csc/instructors/classlists/$CDF $TMP || die scp failed
 set -
 
-sort $TMP > $TMP_SORT || die sort $TMP failed
+#sort by utorid
+sort --field-separator=, --key=3,3 --key=1,1 $TMP |  sed -e "s/,9/,09/" | sed -e "s/LEC //"  > $TMP_SORT || die sort $TMP failed
 
 set -x
-diff $CLASSLIST $TMP_SORT 
+diff $TMP_SORT $CLASSLIST 
 set -
 
+echo too complicated\? try:
+echo xdiff $TMP_SORT  $CLASSLIST
+
+read -p "continue" JUNK
+
+echo
+echo utorids of drops:
+echo
+
+comm -13 $TMP_SORT  $CLASSLIST | cut -d, -f1
+
+echo
+echo lines from $EMPTY:
+echo
+
+comm -13 $TMP_SORT  $CLASSLIST | cut -d, -f1 | xargs -I % grep % $EMPTY
+
+read -p "comm -13.. (drops)  >" junk
+comm -13 $TMP_SORT  $CLASSLIST 
+echo run drops.sh next to mark these student as having dropped
+echo
+read -p "comm -23.. (new students) ..  >" junk
+comm -23 $TMP_SORT  $CLASSLIST 
+
+read -p "added student lines in classlist formated as for empty grades file format >" junk
+echo
+
+comm -23 $TMP_SORT  $CLASSLIST \
+	 | awk -F, '{printf "%s    %s%s,%s,%s,TAx,ROOM,%s,%s,%s\n", $2, $4,$5,$1,$3,$4,$5,$6}' \
+	 | sed -e "s/LEC //" 
+
+echo
+echo copy/paste following into empty file
+echo
 read -p "look right? interrupt out now. enter to cp new classlist to $CLASSLIST  >" junk
+
 bf=$(backfilename.sh $CLASSLIST)
 mv $CLASSLIST $bf || die failed to backup $CLASSLIST
-mv $TMP_SORT $CLASSLIST || die failed to mv $TMP_SORT $CLASSLIST 
-ls -l $bf
+mv -i $TMP_SORT $CLASSLIST || die failed to mv $TMP_SORT $CLASSLIST 
 echo backup saved in $bf
+ls -l $bf
+
 echo "if students were ADDED edit grades files by hand. else just run drops.sh"
+echo xdiff $TMP_SORT  $CLASSLIST 
 
 
 
