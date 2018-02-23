3ca27c65c0ffcc82294a393302d020dbd910de6c
Mathew Zaleski <matz@zaleski.ca>
2018-02-23 14:27:03 -0500

    factor utorid_map (completion map) into AppState

--- prompt_for_input_string_with_completions_curses.py
+++ prompt_for_input_string_with_completions_curses.py
@@ -2,10 +2,12 @@
 
 class AppState:
     "store stuff"
-    def __init__(self, initial_message):
+    def __init__(self, initial_message,utorid_map ):
         self.initial_message = initial_message
         self.query = ''
         self.ix = 1
+        self.utorid_map  = utorid_map 
+        #TODO: add utorid_map and methods like longest_prefix, etc
         
     def clear_query(self):
         "clear line"
@@ -21,6 +23,9 @@ class AppState:
         self.query = q
         self.ix = len(q)+1
 
+    def query_in_completion_map(self):
+        return self.query in utorid_map.keys()        
+
     def is_query_empty(self):
         return self.ix == 1
     
@@ -28,6 +33,60 @@ class AppState:
         "bs = back space = remove char from RH end of query"
         self.query = self.query[:-1]
         self.ix = len(self.query)+1
+    
+    def longest_common_prefix(self):
+        """
+        return a tuple.. (isWholeCompletion,aStringPrefix) the flag tells if the prefix is the whole completion, the string is the prefix
+        """
+        if len(self.query) == 0:
+            return (False,None)
+        l = []
+
+        for o in self.utorid_map.keys():
+            if o.startswith(self.query):
+                l.append(o)
+        # forget it none of them start with query.
+        if len(l) == 0:
+            return (False,None)
+
+        # an arbitrary utorid which startswith query
+        an_opt = l[0] 
+
+        # search for the longest prefix that all of l share.
+        # can't be longer than length of an_opt, so only search that far
+
+        prefix = self.query
+        prev_prefix = ''
+        verbose = False
+        for ix in range(len(self.query),len(an_opt)+1):
+            prev_prefix = prefix
+            prefix = an_opt[:ix] #save longest so far..
+            assert(len(prefix)==ix)
+            if verbose: print("\r\nix",ix,"prefix", prefix)
+            for o in l:
+                if not o.startswith(prefix):
+                    if verbose: print("\r\nnope, prefix too long",prefix, prev_prefix)
+                    assert(prev_prefix)
+                    return (False,prev_prefix)
+
+        #here if entire an_opt is longest common prefix
+        if verbose: print("\r\nhere an_opt,prefix,prev_prefix",an_opt,prefix, prev_prefix)
+        return (True,prefix)
+
+    def query_is_unique(self):
+        if self.query_in_completion_map():
+            return True
+        # completion is the longest prefix of the all the utorid's starting with query --
+        (is_whole,completion) = self.longest_common_prefix()
+        if is_whole:
+             app_state.set_query(completion)
+             return True
+        # might not be complete, but still may uniquely identifies someone?
+        n=0
+        for id in self.utorid_map:
+            if id.startswith(self.query):
+                n += 1
+        return n == 1
 
 import curses.ascii
 class AppViewer:
@@ -128,11 +187,11 @@ class AppViewer:
         self.stdscr.move(self.height+1,1)
         self.stdscr.clrtoeol()
 
-    def refresh_view(self,utorids,prompt,c):
+    def refresh_view(self,prompt,c):
         "redraw status and completion areas"
         self.update_status_line(c)
         self.erase_completions()
-        self.show_completions(utorids)
+        self.show_completions(self.app_state.utorid_map)
         self.stdscr.move(self.height,1)
         self.stdscr.clrtoeol()
         self.stdscr.addstr(prompt)
@@ -146,52 +205,14 @@ class AppViewer:
         return c == curses.ascii.LF
     def is_tab(self,c):
         return c == curses.ascii.TAB
-    def is_nak(self,c):
+    def is_kill_input(self,c):
         # this is getting on thin ice. stty determines which character "kills" all input ^U for me..
         return c == curses.ascii.NAK
     def is_bs(self,c):
         return (c == curses.ascii.BS) or (c == curses.ascii.DEL) # backspace on my keyboard??
     
-def longest_common_prefix(utorid_map,query):
-    """
-    return a tuple.. (flagTrueIfWholeCompletion,aStringPrefix) the flag tells if the prefix is the whole completion, the string is the prefix
-    """
-    if len(query) == 0:
-        return (False,None)
-    l = []
 
-    for o in utorid_map.keys():
-        if o.startswith(query):
-            l.append(o)
-    # forget it none of them start with query.
-    if len(l) == 0:
-        return (False,None)
-
-    # an arbitrary utorid which startswith query
-    an_opt = l[0] 
-
-    # search for the longest prefix that all of l share.
-    # can't be longer than length of an_opt, so only search that far
-
-    prefix = query
-    prev_prefix = ''
-    verbose = False
-    for ix in range(len(query),len(an_opt)+1):
-        prev_prefix = prefix
-        prefix = an_opt[:ix] #save longest so far..
-        assert(len(prefix)==ix)
-        if verbose: print("\r\nix",ix,"prefix", prefix)
-        for o in l:
-            if not o.startswith(prefix):
-                if verbose: print("\r\nnope, prefix too long",prefix, prev_prefix)
-                assert(prev_prefix)
-                return (False,prev_prefix)
-
-    #here if entire an_opt is longest common prefix
-    if verbose: print("\r\nhere an_opt,prefix,prev_prefix",an_opt,prefix, prev_prefix)
-    return (True,prefix)
-
-def prompt_for_input_string_with_completions_curses(prompt,height,utorid_map,initial_warning_message):
+def prompt_for_input_string_with_completions_curses(prompt,height,completion_map,initial_warning_message):
     """
     beware: first (as in novice) attempt at curses programming.
     Prompt for a character, and show completions matching the query so far.
@@ -199,8 +220,8 @@ def prompt_for_input_string_with_completions_curses(prompt,height,utorid_map,ini
     """
     import curses.ascii
 
-    app_state = AppState(initial_warning_message)
-    av = AppViewer(height,app_state)
+    app_state = AppState(initial_warning_message,completion_map)
+    app_viewer = AppViewer(height,app_state)
 
     # TODO: users terminal setting stty(1) actually determine what special keys do what
     # eg: I like ^C for interrupt and DEL for backspace,
@@ -209,74 +230,57 @@ def prompt_for_input_string_with_completions_curses(prompt,height,utorid_map,ini
     try:
         app_state.clear_query()
         c = ord(' ')
-        av.show_warning_message(initial_warning_message)
-        av.refresh_view(utorid_map,prompt,c)
+        app_viewer.show_warning_message(initial_warning_message)
+        app_viewer.refresh_view(prompt,c)
 
-        #input loop..
         while True:
-            c = av.getch() 
-            av.update_status_line(c)
-            av.clear_warning_message()
+            c = app_viewer.getch() 
+            app_viewer.update_status_line(c)
+            app_viewer.clear_warning_message()
                 
-            if av.is_lf(c): #### end of line ish
-                if app_state.query in utorid_map.keys():
-                    break  # done.. query is exactly one of the utorids..
-
-                # completion is the longest prefix of the all the utorid's starting with query --
-                (is_whole,completion) = longest_common_prefix(utorid_map,app_state.query)
-
-                if is_whole:
-                    app_state.set_query(completion)
+            if app_viewer.is_lf(c): #### end of line ish
+                if app_state.query_is_unique():
+                    break;
+                msg = "query " + "`"  + app_state.query + "' does not identify a unique utorid.. hit enter again to return it anyway: "
+                app_viewer.show_warning_message(msg)
+                app_viewer.beep()
+                c = app_viewer.getch()
+                if app_viewer.is_lf(c):
                     break
-
-                # might not be complete, but still may uniquely identifies someone?
-                n=0
-                for id in utorid_map:
-                    if id.startswith(app_state.query):
-                        n += 1
-                        
-                if n != 1: 
-                    msg = "query " + "`"  + app_state.query + "' does not identify a unique utorid.. hit enter again to return it anyway: "
-                    av.show_warning_message(msg)
-                    av.beep()
-                    c = av.getch()
-                    if av.is_lf(c):
-                        break
-                    else:
-                        # other than LF want to continue as normal.. ungetc..
-                        av.ungetch(c)
-                        av.clear_warning_message()
+                else:
+                    # other than LF want to continue as normal.. ungetc..
+                    app_viewer.ungetch(c)
+                    app_viewer.clear_warning_message()
             
-            elif  av.is_eof(c): #end of file'ish
+            elif  app_viewer.is_eof(c): #end of file'ish
                 break
             
-            elif av.is_tab(c):
-                (flg,completion) = longest_common_prefix(utorid_map,app_state.query)
+            elif app_viewer.is_tab(c):
+                (flg,completion) = app_state.longest_common_prefix()
                 if completion:
                     app_state.set_query(completion)
                     c = ord(' ')
                 else:
                     curses.beep()
 
-            elif av.is_bs(c): #backspace
+            elif app_viewer.is_bs(c):
                 if app_state.is_query_empty():
-                    av.beep()
+                    app_viewer.beep()
                 else:
                     app_state.back_space()
 
-            elif av.is_nak(c): # control-u
-                av.beep()
+            elif app_viewer.is_kill_input(c):
+                app_viewer.beep()
                 app_state.clear_query()
-            
             else:
                 app_state.add_char_to_query(c)
                 
-            av.refresh_view(utorid_map,prompt,c)
+            app_viewer.refresh_view(prompt,c)
 
         ############# end input loop..
 
-        av.cleanup()
-        if av.is_eof(c): #ya think? what else could they mean by entering ^d ?
+        app_viewer.cleanup()
+        if app_viewer.is_eof(c): #ya think? (when i hit ^d i mean i'm finished with input.)
             return None
         else:
             return app_state.query
