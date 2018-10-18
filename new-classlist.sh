15e271cf3569d138be1fe04ecf8b7fb0239eedf6
Mathew Zaleski <matz@zaleski.ca>
2018-10-18 11:58:50 -0400

    toss -empty grades file stuff

--- new-classlist.sh
+++ new-classlist.sh
@@ -12,7 +12,8 @@ test ! -z "$SESSION" || die SESSION env not found.. should be defined in ./dot.s
 test -d $GBIN || die no grades bin in $GBIN
 
 CDF="CSC"$COURSE$SESSION
-CLASSLIST=$CDF-classlist
+#CLASSLIST=$CDF-classlist
+CLASSLIST=$CDF-cdf.csv
 EMPTY=$CDF-empty
 
 test -f $CLASSLIST || die $0: cannot find class list $CLASSLIST
@@ -25,7 +26,20 @@ scp matz@cdf.utoronto.ca:/u/csc/instructors/classlists/$CDF $TMP || die scp fail
 set -
 
 #sort by utorid
-sort --field-separator=, --key=3,3 --key=1,1 $TMP |  sed -e "s/,9/,09/" | sed -e "s/LEC //"  > $TMP_SORT || die sort $TMP failed
+#sort --field-separator=, --key=3,3 --key=1,1 $TMP |  sed -e "s/,9/,09/" | sed -e "s/LEC //"  > $TMP_SORT || die sort $TMP failed
+
+# sort by section then utorid
+#sort --field-separator=, --key=3,3 --key=1,1 $TMP   > $TMP_SORT || die sort $TMP failed
+
+# nevermind section
+sort --field-separator=,  --key=1,1 $TMP   > $TMP_SORT || die sort $TMP failed
+
+echo drops
+comm -13 $TMP_SORT  $CLASSLIST | wc -l
+echo adds
+comm -23 $TMP_SORT  $CLASSLIST | wc -l
+
+read -p 'diff previous and newly downloaded class list' junk
 
 set -x
 diff $TMP_SORT $CLASSLIST 
@@ -42,30 +56,14 @@ echo
 
 comm -13 $TMP_SORT  $CLASSLIST | cut -d, -f1
 
-echo
-echo lines from $EMPTY:
-echo
-
-comm -13 $TMP_SORT  $CLASSLIST | cut -d, -f1 | xargs -I % grep % $EMPTY
-
 read -p "comm -13.. (drops)  >" junk
 comm -13 $TMP_SORT  $CLASSLIST 
-echo run drops.sh next to mark these student as having dropped
+
+
+
 echo
 read -p "comm -23.. (new students) ..  >" junk
 comm -23 $TMP_SORT  $CLASSLIST 
-
-read -p "added student lines in classlist formated as for empty grades file format >" junk
-echo
-
-comm -23 $TMP_SORT  $CLASSLIST \
-	 | awk -F, '{printf "%s    %s%s,%s,%s,TAx,ROOM,%s,%s,%s\n", $2, $4,$5,$1,$3,$4,$5,$6}' \
-	 | sed -e "s/LEC //" 
-
-echo
-echo copy/paste following into empty file
-echo
-read -p "look right? interrupt out now. enter to cp new classlist to $CLASSLIST  >" junk
 
 bf=$(backfilename.sh $CLASSLIST)
 mv $CLASSLIST $bf || die failed to backup $CLASSLIST
